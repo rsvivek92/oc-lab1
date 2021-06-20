a99afeee9f5f9168772cde91b32bbdf1de986330
Ranka Surender <vrankasurender@deloitte.com>
2021-06-20 13:22:53 +0530

    first commit

---
+++ php/config.php
@@ -0,0 +1,22 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password)
+define('DB_SERVER', 'mysql-db');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+*/
+$DB_SERVER = getenv('ENV_DB_SERVER');
+$DB_USERNAME = getenv('ENV_DB_USERNAME');
+$DB_PASSWORD = getenv('ENV_DB_PASSWORD');
+$DB_NAME = getenv('ENV_DB_NAME');
+
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
